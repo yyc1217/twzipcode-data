3fbc72b561c30b4b5bd3f7b0ef28aecfdcf231df
Yeh-Yung <yyc1217@gmail.com>
2017-03-26 12:03:45 +0800

    Convert zh-tw to UTF-8 for better experience.

--- src/zh-tw/counties.js
+++ src/zh-tw/counties.js
@@ -1,90 +1,90 @@
 module.exports = [
   {
-    id: '\u81fa\u5317\u5e02',
-    name: '\u81fa\u5317\u5e02'
+    id: '臺北市',
+    name: '臺北市'
   },
   {
-    id: '\u57fa\u9686\u5e02',
-    name: '\u57fa\u9686\u5e02'
+    id: '基隆市',
+    name: '基隆市'
   },
   {
-    id: '\u65b0\u5317\u5e02',
-    name: '\u65b0\u5317\u5e02'
+    id: '新北市',
+    name: '新北市'
   },
   {
-    id: '\u5b9c\u862d\u7e23',
-    name: '\u5b9c\u862d\u7e23'
+    id: '宜蘭縣',
+    name: '宜蘭縣'
   },
   {
-    id: '\u65b0\u7af9\u5e02',
-    name: '\u65b0\u7af9\u5e02'
+    id: '新竹市',
+    name: '新竹市'
   },
   {
-    id: '\u65b0\u7af9\u7e23',
-    name: '\u65b0\u7af9\u7e23'
+    id: '新竹縣',
+    name: '新竹縣'
   },
   {
-    id: '\u6843\u5712\u5e02',
-    name: '\u6843\u5712\u5e02'
+    id: '桃園市',
+    name: '桃園市'
   },
   {
-    id: '\u82d7\u6817\u7e23',
-    name: '\u82d7\u6817\u7e23'
+    id: '苗栗縣',
+    name: '苗栗縣'
   },
   {
-    id: '\u81fa\u4e2d\u5e02',
-    name: '\u81fa\u4e2d\u5e02'
+    id: '臺中市',
+    name: '臺中市'
   },
   {
-    id: '\u5f70\u5316\u7e23',
-    name: '\u5f70\u5316\u7e23'
+    id: '彰化縣',
+    name: '彰化縣'
   },
   {
-    id: '\u5357\u6295\u7e23',
-    name: '\u5357\u6295\u7e23'
+    id: '南投縣',
+    name: '南投縣'
   },
   {
-    id: '\u5609\u7fa9\u5e02',
-    name: '\u5609\u7fa9\u5e02'
+    id: '嘉義市',
+    name: '嘉義市'
   },
   {
-    id: '\u5609\u7fa9\u7e23',
-    name: '\u5609\u7fa9\u7e23'
+    id: '嘉義縣',
+    name: '嘉義縣'
   },
   {
-    id: '\u96f2\u6797\u7e23',
-    name: '\u96f2\u6797\u7e23'
+    id: '雲林縣',
+    name: '雲林縣'
   },
   {
-    id: '\u81fa\u5357\u5e02',
-    name: '\u81fa\u5357\u5e02'
+    id: '臺南市',
+    name: '臺南市'
   },
   {
-    id: '\u9ad8\u96c4\u5e02',
-    name: '\u9ad8\u96c4\u5e02'
+    id: '高雄市',
+    name: '高雄市'
   },
   {
-    id: '\u6f8e\u6e56\u7e23',
-    name: '\u6f8e\u6e56\u7e23'
+    id: '澎湖縣',
+    name: '澎湖縣'
   },
   {
-    id: '\u5c4f\u6771\u7e23',
-    name: '\u5c4f\u6771\u7e23'
+    id: '屏東縣',
+    name: '屏東縣'
   },
   {
-    id: '\u81fa\u6771\u7e23',
-    name: '\u81fa\u6771\u7e23'
+    id: '臺東縣',
+    name: '臺東縣'
   },
   {
-    id: '\u82b1\u84ee\u7e23',
-    name: '\u82b1\u84ee\u7e23'
+    id: '花蓮縣',
+    name: '花蓮縣'
   },
   {
-    id: '\u91d1\u9580\u7e23',
-    name: '\u91d1\u9580\u7e23'
+    id: '金門縣',
+    name: '金門縣'
   },
   {
-    id: '\u9023\u6c5f\u7e23',
-    name: '\u9023\u6c5f\u7e23'
+    id: '連江縣',
+    name: '連江縣'
   }
 ]
